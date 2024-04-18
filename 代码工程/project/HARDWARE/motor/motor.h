da7d7816c8d166a3da2d6d228f86a7ec4afc036e
CrazyEggshell <145254036+CrazyEggshell@users.noreply.github.com>
2024-04-18 10:34:09 +0800

    Delete 代码工程 directory

--- 代码工程/project/HARDWARE/motor/motor.h
+++
@@ -1,7 +0,0 @@
-#ifndef MOTOR_H
-#define MOTOR_H	 
-#include "sys.h"
-#define MOTOR1 PAout(5)
-#define MOTOR2 PAout(6)
-void MOTOR_Init(void);
-#endif
--- 代码工程/project/HARDWARE/rain/rain.c
+++
@@ -1,9 +0,0 @@
-#include "rain.h"
-void RAIN_Init(void)
-{
- GPIO_InitTypeDef  RAIN_in;	
- RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);	
- RAIN_in.GPIO_Pin = GPIO_Pin_4;				
- RAIN_in.GPIO_Mode = GPIO_Mode_IN_FLOATING; 		
- GPIO_Init(GPIOB, &RAIN_in);	 
-}
